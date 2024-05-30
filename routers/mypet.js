4db45c1a0248f8f0e9029260c890c3ba2eb0a0c6
jiyeon32 <kangjy003482@gmail.com>
2024-05-30 15:20:06 +0900

    feat:mypet.js

---
+++ routers/mypet.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/mypet', function (req, res) {
+    res.render('mypet'); // singup.ejs 파일을 렌더링
+});
+
+
+module.exports = router;
